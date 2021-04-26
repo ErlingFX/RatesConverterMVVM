7b19b473477b1fd71405463b999c6f011baac5eb
ErlingFX <75131227+ErlingFX@users.noreply.github.com>
2021-04-26 13:07:42 +0400

    Initial Commit

---
+++ RatesConverterMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RatesConverterMVVM
+//
+//  Created by Alexander Nazarov on 26.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
